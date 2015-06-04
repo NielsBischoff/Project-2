0d6996ac64d148799868fd8176a6e374a16dafb6
nielsbischoff <niels.bischoff@gmail.com>
2015-06-03 20:49:58 -0700

    Sessions controller and view working

---
+++ doday_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController <ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+    user = User.find_by(email: params[:login][:email])
+
+    if user && user.authenticate(params[:login][:password])
+      session[:user_id] = user.id.to_s
+      redirect_to user_path
+    else
+      render :new
+    end
+
+  end
+
+  def destroy
+      sessions.delete(:user_id)
+      redirect_to login_path
+  end
+
+end
