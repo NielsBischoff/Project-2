1d0c677f12f8b2d4a24c2dc30908869b223f88fa
nielsbischoff <niels.bischoff@gmail.com>
2015-06-05 20:12:30 -0700

    doday_app

--- doday_app/app/controllers/goals_controller.rb
+++ doday_app/app/controllers/goals_controller.rb
@@ -1,31 +1,31 @@
 class GoalsController < ApplicationController
 
-    def show
-      @goal = Goal.find(params[:id])
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(user_params)
+    if @goal.save
+      redirect_to @task
+      flash[:success] = "You defined a goal on your path to success!"
+      redirect_to @goal
+    else
+      render 'new'
     end
+  end
 
-    def new
-      @goal = Goal.new
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:goal).permit(:task_description, :time_frame, :priority, :completed)
     end
-
-    def create
-      @goal = Goal.new(user_params)
-      if @goal.save
-        redirect_to @task
-        flash[:success] = "You defined a goal on your path to success!"
-        redirect_to @goal
-      else
-        render 'new'
-      end
-    end
-
-    def edit
-      @goal = Goal.find(params[:id])
-    end
-
-    private
-
-      def user_params
-        params.require(:goal).permit(:task_description, :time_frame, :priority, :completed)
-      end
 end
